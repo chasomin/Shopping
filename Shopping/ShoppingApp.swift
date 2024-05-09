2257dcc24d3313feec3cb45bde00d8a68dcb91c3
chasomin <somin49@naver.com>
2024-05-09 16:20:56 +0900

    Initial Commit

---
+++ Shopping/ShoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingApp.swift
+//  Shopping
+//
+//  Created by 차소민 on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
